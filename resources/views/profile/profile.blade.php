e5ce7af3a2422e66895b880fc6a1c89fe185d35d
umairdev <merrymalilk5050@gmail.com>
2025-07-21 05:08:36 +0500

    title edit

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
+  {{-- Edit Title --}}
   <title>Profile Bio - SyLica</title>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
